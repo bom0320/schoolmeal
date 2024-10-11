c6c775331cc54c40aae52b023963c5a5b5030b7c
bom0320 <167315197+bom0320@users.noreply.github.com>
2024-10-11 13:59:54 +0900

    update: error correction

--- src/App.js
+++ src/App.js
@@ -4,31 +4,31 @@ import useFetchMeals from './useFetchMeals';
 
 const App = () => {
   const [mealType, setMealType] = useState('조식');  // 기본값을 '조식'으로 설정
-  const [currentDate, setCurrentDate] = useState(new Date());
-
+  const [currentDate, setCurrentDate] = useState(new Date());  // 현재 날짜를 상태로 설정
   const API_KEY = 'e2e38e8c19fa49f8b9050101d8d386a7'; 
   const SD_SCHUL_CODE = '7380292';  
   const ATPT_OFCDC_SC_CODE = 'F10'; 
 
   // 날짜를 yyyyMMdd 형식으로 변환하는 함수
-  const formatDate = (date) => {  // 'data'가 아니라 'date'로 변경
+  const formatDate = (date) => {  
     let day = String(date.getDate()); 
-    let month = String(date.getMonth() + 1);  // 월은 0부터 시작하므로 +1 필요
+    let month = String(date.getMonth() + 1); 
     let year = String(date.getFullYear());
     return year + month.padStart(2, '0') + day.padStart(2, '0');
   };
 
-  const today = formatDate(currentDate);  // 여기서 currentDate를 전달
+  // 날짜 변경 함수
+  const changeDate = (days) => {
+    const newDate = new Date(currentDate);
+    newDate.setDate(newDate.getDate() + days);  // days 값을 더하거나 빼서 새로운 날짜 계산
+    setCurrentDate(newDate);  // 새로운 날짜로 상태 업데이트
+  };
+
+  const today = formatDate(currentDate);  // 변경된 날짜를 yyyyMMdd 형식으로 변환
 
   const API_URL = `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${API_KEY}&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}&SD_SCHUL_CODE=${SD_SCHUL_CODE}&MLSV_YMD=${today}`;
   
   const { meals, loading, error } = useFetchMeals(API_URL);
-
-  const changeDate = (days) => {
-    const newDate = new Date(currentDate);
-    newDate.setDate(currentDate.getDate() + days);
-    setCurrentDate(newDate); // 날짜 상태 업데이트
-  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -46,7 +46,7 @@ const App = () => {
       <div className='header'>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlpx5M5t6y0B6J6jt0f2XhMpCjC5epkyOfKQ&s' alt='학교 사진' width="288" height="175"/>
         <h1>광주소프트웨어마이스터고등학교 오늘의 급식판</h1>
-        <h2>{currentDate.toLocaleDateString()}</h2> 
+        <h2>{today}</h2>  
 
         <button onClick={() => setMealType('조식')}>조식</button>
         <button onClick={() => setMealType('중식')}>중식</button>
--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const MealItem = ({ meal }) => {
     return (
         <li>
-            <h2>{meal.MLSV_YMD}</h2>
-            <p>메뉴: {meal.DDISH_NM}</p>
+            <h2>{meal.MMEAL_SC_NM}</h2>  {/* 조식, 중식, 석식 출력 */}
+            <p>
+              <span dangerouslySetInnerHTML={{ __html: meal.DDISH_NM.replace(/\n/g, '<br/>') }} />
+            </p>
             <p>칼로리: {meal.CAL_INFO}</p>
             <hr />
         </li>
